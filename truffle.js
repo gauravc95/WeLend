9f2a38a3e28d8cee12bac5a9e52a582665161a1a
Gaurav <gaurav@chaudhari.com>
2019-03-27 23:07:36 +0530

    UI update contract working fine with rinleby nw

--- truffle.js
+++ truffle.js
@@ -1,5 +1,9 @@
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register')
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+
+var mnemonic = "accuse shine chimney access aware engine neither prevent crazy decline round rookie glare diamond define";
 
 module.exports = {
   networks: {
@@ -7,6 +11,14 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: '*' // Match any network id
-    }
-  }
+    },
+  
+  rinkeby: {
+    provider: function() {
+      return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/256372af6edc458fb98b69114066e503")
+    },
+    network_id: 4,
+    gas: 4612388 // Gas limit used for deploys
+  } 
+},
 }
